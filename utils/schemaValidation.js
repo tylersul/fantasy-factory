d641bbb9f2a5f2b1b34812f9878b7e4656863827
Franchise <tylerreedsullivan@gmail.com>
2021-12-20 15:20:23 -0600

    Joi validation for server-side on seasons

--- utils/schemaValidation.js
+++ utils/schemaValidation.js
@@ -7,4 +7,13 @@ module.exports.managerJoiSchema = Joi.object({
     losses: Joi.number().required(),
     description: Joi.string().required()
   }).required()
-})
+});
+
+module.exports.seasonJoiSchema = Joi.object({
+  season: Joi.object({
+    year: Joi.string().required(),
+    champion: Joi.string().required(),
+    loser: Joi.string().required(),
+    description: Joi.string().required()
+  }).required()
+});
