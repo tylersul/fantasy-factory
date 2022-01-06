7471e04d234705fe01eaf1c0a43ad27565f0d9c6
Franchise <tylerreedsullivan@gmail.com>
2022-01-06 14:11:36 -0600

    Environment deployment logic.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
+
 // Imports
 const express              = require('express'),
       app                  = express(),
