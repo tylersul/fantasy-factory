bdbfd325c682b270f90119f7ea1e32d561d17296
Franchise <tylerreedsullivan@gmail.com>
2022-01-06 14:12:28 -0600

    Refactored routes to use router.route

--- routes/managers.js
+++ routes/managers.js
@@ -1,86 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync           = require('../utils/catchAsync');
-const ExpressError         = require('../utils/ExpressError');
+const managerController = require('../controllers/managers');
 const { isLoggedIn, isAuthor, validateManager }       = require('../utils/middleware');
-const Manager              = require('../models/manager');
+
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 // ================================================================== //
 // ====================== Routes ==================================== //
 // ================================================================== //
-// GET /managers - View all managers
-router.get('/managers', catchAsync(async (req, res) => {
-  const managers = await Manager.find({});
-  res.render("managers/index", { managers });
-}));
-
+router.route('/managers')
+      .get(catchAsync(managerController.index))
+      .post(isLoggedIn, upload.array('image'), validateManager, catchAsync(managerController.postManager));
 // GET /managers/new - Get create new manager form
-router.get('/managers/new', isLoggedIn, (req, res) => {
-  res.render('managers/new');
-});
+router.get('/managers/new', isLoggedIn, managerController.getNewForm);
+
+router.route('/managers/:id')
+      .get(catchAsync(managerController.getManager))
+      .put(isLoggedIn, isAuthor, upload.array('image'), validateManager, catchAsync(managerController.putManagers))
+      .delete(isLoggedIn, isAuthor, catchAsync(managerController.deleteManager));
+
+// GET /managers - View all managers
+// router.get('/managers', catchAsync(managerController.index));
 
 // POST /managers - Create new manager
-router.post('/managers', validateManager, catchAsync(async (req, res) => {
-  // Stops API calls from posting new managers without correct data
-  // if (!req.body.campground) throw new ExpressError('Invalid manager data', 400);
-
-  const manager = new Manager(req.body.manager);
-  manager.author = req.user._id;
-  await manager.save();
-  req.flash('success', 'Successfully created new manager.');
-  res.redirect(`/managers/${manager._id}`);
-}));
+// Stops API calls from posting new managers without correct data
+// router.post('/managers', validateManager, catchAsync(managerController.postManager));
 
 // GET /managers/:id - View specific manager
-router.get('/managers/:id', catchAsync(async (req, res) => {
-  const manager = await Manager.findById(req.params.id).populate('author');
-  if (!manager) {
-    req.flash('error', 'Manager not found.');
-    return res.redirect('/managers');
-  }
-  res.render('managers/show', { manager });
-}));
+// router.get('/managers/:id', catchAsync(managerController.getManager));
 
 // PUT /managers/:id - Update specific manager
-router.put('/managers/:id', isLoggedIn, validateManager, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  console.log(id)
-  const manager = await Manager.findByIdAndUpdate(id, { ...req.body.manager });
-  req.flash('success', 'Successfully updated manager.');
-  res.redirect(`/managers/${manager._id}`)
-})); 
-
-// GET /managers/:id/edit - Get update manager form
-router.get('/managers/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req,res) => {
-
-  const manager = await Manager.findById(req.params.id);
-
-  if (!manager) {
-    req.flash('error', 'Manager not found.');
-    return res.redirect('/managers');
-  }
-
-  res.render('managers/edit', { manager })
-}));
-
-// PUT /seasons/:id - Update specific season
-router.put('/managers/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  console.log(id)
-  const season = await Season.findByIdAndUpdate(id, { ...req.body.season });
-  console.log(season._id)
-  res.redirect(`/seasons/${season._id}`)
-}));
+// router.put('/managers/:id', isLoggedIn, isAuthor, validateManager, catchAsync(managerController.putManagers)); 
 
 // DELETE /managers/:id - Delete specific manager
-router.delete('/managers/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  console.log(req.params);
-  console.log(id);
-  await Manager.findByIdAndDelete(id);
-  req.flash('success', 'Successfully deleted manager.');
-  res.redirect('/managers');
-}));
+// router.delete('/managers/:id', isLoggedIn, isAuthor, catchAsync(managerController.deleteManager));
+
+// GET /managers/:id/edit - Get update manager form
+router.get('/managers/:id/edit', isLoggedIn, isAuthor, catchAsync(managerController.getEditForm));
+
 
 
 // ================================================================== //
